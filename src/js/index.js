5e931664747b82fd04307d0dd70bc5f9f4f6fc1e
justinok <38015681+justinok@users.noreply.github.com>
2022-08-07 21:49:01 -0500

    All get for post and comments finished!!!
    oh yeah all finished in the same page, not style yet

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,38 @@ console.log("te odio CORS");
 let posts;
 getAllPostsFromBackend().then(response => {
     posts = response;
-    console.log(posts);
+    materializePosts(posts);
 });
+function materializePosts(posts) {
+    const divRoot = document.querySelector('#root');
+    posts.forEach(post => renderPost(post, divRoot));
+    console.log(posts);
+    console.log(divRoot);
+}
+function renderPost(post, divRoot) {
+    // Ipost[]  ???
+    const singlePostContainer = document.createElement('div');
+    singlePostContainer.className = `single_post_container-${post.id}`;
+    singlePostContainer.classList.add('single_post_container');
+    const singlePostContent = `
+     <h2 class="single-post-name-${post.id}">${post.title}</h2>
+     <p class="message-${post.id}">${post.message}</p>
+     `;
+    singlePostContainer.innerHTML = singlePostContent;
+    materializeComments(post.comments, singlePostContainer);
+    divRoot.append(singlePostContainer);
+}
+function materializeComments(comments, postContainter) {
+    comments.forEach(comment => renderComment(comment, postContainter));
+}
+function renderComment(comment, postContainter) {
+    const singleCommentContainer = document.createElement('div');
+    singleCommentContainer.className = `single_comment_container_${comment.id}`;
+    singleCommentContainer.classList.add(`single_comment_container`);
+    const singleCommentContent = `
+    <h3 class ="comment_title_${comment.id}">Anonim Comment:</h3>
+    <p class ="comment_message_${comment.id}">${comment.message}</p>
+    `;
+    singleCommentContainer.innerHTML = singleCommentContent;
+    postContainter.append(singleCommentContainer);
+}
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,4 @@
-import { Ipost } from "./models/models.js";
+import { Ipost, Icomment} from "./models/models.js";
 
 import { getAllPostsFromBackend } from "./requests/asyncRequests.js";
 
@@ -8,5 +8,46 @@ console.log("te odio CORS");
 let posts:Ipost[];
 getAllPostsFromBackend().then(response => {
     posts = response
-    console.log(posts);
+    materializePosts(posts);
+    
 } )
+
+function materializePosts(posts:Array<Ipost>){
+    const divRoot = document.querySelector('#root') as
+    HTMLDivElement;
+    posts.forEach(post => renderPost(post, divRoot))
+    console.log(posts)
+    console.log(divRoot)
+
+}
+
+function renderPost(post:Ipost, divRoot:HTMLDivElement){
+    // Ipost[]  ???
+    
+    const singlePostContainer = document.createElement('div');
+    singlePostContainer.className = `single_post_container-${post.id}`
+    singlePostContainer.classList.add('single_post_container') 
+    const singlePostContent = `
+     <h2 class="single-post-name-${post.id}">${post.title}</h2>
+     <p class="message-${post.id}">${post.message}</p>
+     `
+     singlePostContainer.innerHTML = singlePostContent;
+     materializeComments(post.comments, singlePostContainer)
+     divRoot.append(singlePostContainer)
+}
+
+function materializeComments(comments:Icomment[], postContainter:HTMLDivElement ){
+    comments.forEach(comment => renderComment(comment, postContainter))
+}
+
+function renderComment(comment:Icomment, postContainter:HTMLDivElement){
+    const singleCommentContainer:HTMLDivElement = document.createElement('div')
+    singleCommentContainer.className = `single_comment_container_${comment.id}`
+    singleCommentContainer.classList.add(`single_comment_container`)
+    const singleCommentContent:string =`
+    <h3 class ="comment_title_${comment.id}">Anonim Comment:</h3>
+    <p class ="comment_message_${comment.id}">${comment.message}</p>
+    `
+    singleCommentContainer.innerHTML = singleCommentContent;
+    postContainter.append(singleCommentContainer)
+}
